3211a0844bd8b910b8a0a41bb2725e1ca7bc88ec
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-03-19 15:20:14 +0700

    update

--- theme/theme-thong-tin-lien-he-version-2-0/js/main.js
+++ theme/theme-thong-tin-lien-he-version-2-0/js/main.js
@@ -1,34 +1,15 @@
+// loading
 $(".loader").delay(1000).fadeOut("fast");
-function showthongbao(str) {
-    $("#thongbaostr").html(str);
-    $("#thongbao").modal("show");
-}
+// open link
 function openlink(url) {
-    window.open(url, "_blank").focus();
+	window.open(url, "_blank").focus();
 }
-
-function show_alert(str) {
-    $("#alert").css("display", "inline");
-    $("#alert").addClass("animate__backInLeft");
-    setTimeout(() => {
-        $("#alert").removeClass("animate__backInLeft");
-        $("#alert").addClass("animate__backOutLeft");
-    }, 3000);
-    setTimeout(() => {
-        $("#alert").removeClass("animate__backOutLeft");
-        $("#alert").removeClass("animate__backInLeft");
-        $("#alert").css("display", "none");
-    }, 3000);
-}
+// click copy
 function copytext(str) {
-    const el = document.createElement("textarea");
-    el.value = str;
-    el.setAttribute("readonly", "");
-    el.style.position = "absolute";
-    el.style.left = "-9999px";
-    document.body.appendChild(el);
-    el.select();
-    document.execCommand("copy");
-    document.body.removeChild(el);
-    showthongbao("Đã copy !");
+	const el = document.createElement("textarea");
+	el.value = str;
+	document.body.appendChild(el);
+	el.select();
+	document.execCommand("copy");
+	Swal.fire("Thông báo", "Copy thành công!", "success");
 }
