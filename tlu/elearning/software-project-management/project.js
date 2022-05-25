bdcb62e183a97a3b4b77df9b4a8bc16da332746c
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-05-25 21:28:21 +0700

    Quản lý dự án phần mềm
    Sử dụng 2 user cùng sửa 1 file text. Cùng đẩy lên máy chủ file text đó và giải quyết xung đột!

---
+++ tlu/elearning/software-project-management/project.js
@@ -0,0 +1,13 @@
+const project = {
+	name: "Quản lý dự án phần mềm",
+	instructor: "Trần Quang Duy",
+	member: [
+		{
+			m1: "Lê Anh Đức",
+			m2: "Nguỵ Thị Ngọc Oanh",
+			m3: "Phan Văn Hưng",
+			m4: "Phạm Trung Tá",
+		},
+	],
+	date: "26/05/2022",
+};
