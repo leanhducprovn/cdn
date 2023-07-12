d4771b535e6d853a5c77205bafeabef44d815e70
Le Anh Duc <leanhduc.pro.vn@gmail.com>
2023-07-12 08:10:13 +0700

    Cập nhật thư viện pdftron
    Hoàn thành :100:

--- library/pdftron/samples/viewing/viewing-with-webviewer-office-editor/viewing-with-webviewer-office-editor.js
+++
@@ -1,8 +0,0 @@
-WebViewer(
-  {
-    path: '../../../lib',
-    enableOfficeEditing: true,
-    initialDoc: 'https://pdftron.s3.amazonaws.com/downloads/pl/legal-contract.docx',
-  },
-  document.getElementById('viewer')
-);
